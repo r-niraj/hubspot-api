c087f72ebf6d0e52fe05fe8270095f245945ef9f
Rajnish Niraj <rajnish@blueumbrella.com>
2023-09-22 18:46:32 +0530

    fsdfsdf

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 require('dotenv').config()
 
 const cors = require('cors');
-app.use(express.json());
+app.use(express.json());    
 
 app.use(cors());
 app.use(function (req, res, next) {
