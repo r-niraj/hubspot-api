f68fb8b24d5588199b6c94d638b63eaa432a7c77
Rajnish Niraj <rajnish@blueumbrella.com>
2023-09-22 18:02:07 +0530

    ffsdf

--- app.js
+++ app.js
@@ -6,10 +6,15 @@ const cors = require('cors');
 
 app.use(cors());
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With,Content-Type, Accept,Authorization');
     res.setHeader('Access-Control-Allow-Credentials', true);
+
+    if(req.method==='OPTIONS'){
+            res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+            return res.status(200).json({});
+    }
     next();
 });
 
