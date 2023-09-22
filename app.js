64cc42c44a9d60c87d265d77e2b9e63800ec5953
Rajnish Niraj <rajnish@blueumbrella.com>
2023-09-22 19:27:29 +0530

    dsada

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ app.use(function (req, res, next) {
     if(req.method==='OPTIONS'){
             res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
             res.header('Access-Control-Allow-Origin', '*');
-            res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
             res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With,Content-Type, Accept,Authorization');
             res.header('Access-Control-Allow-Credentials', true);
             return res.status(200).json({});
