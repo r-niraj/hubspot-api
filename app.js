044d7a32d4e14cf9df9cd43ef35c7b5af78aa595
Rajnish Niraj <rajnish@blueumbrella.com>
2023-09-22 20:56:41 +0530

    dsadas

--- app.js
+++ app.js
@@ -6,20 +6,20 @@ app.use(express.json());
 
 
 app.use(cors());
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With,Content-Type, Accept,Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', true);
+// app.use(function (req, res, next) {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With,Content-Type, Accept,Authorization');
+//     res.setHeader('Access-Control-Allow-Credentials', true);
     
-    if(req.method==='OPTIONS'){
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With,Content-Type, Accept,Authorization');
-        res.setHeader('Access-Control-Allow-Credentials', true);
-    }
-    next();
-});
+//     if(req.method==='OPTIONS'){
+//         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//         res.setHeader('Access-Control-Allow-Origin', '*');
+//         res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With,Content-Type, Accept,Authorization');
+//         res.setHeader('Access-Control-Allow-Credentials', true);
+//     }
+//     next();
+// });
 
 app.get("/", (req,resp)=>{
     resp.json({message:"This is message for testing"});
